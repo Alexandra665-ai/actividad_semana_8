42aedc3aa08608b66d29f98e35900796d72ba3c7
Alexandra665-ai <alexandra.m.t.665@gmail.com>
2026-05-13 14:01:58 -0400

    Middleware logger

---
+++ middlewares/logger.js
@@ -0,0 +1,9 @@
+function logger(req, res, next) {
+    const fecha = new Date().toLocaleString();
+
+    console.log(`[${fecha}] ${req.method} ${req.url}`);
+
+    next();
+}
+
+module.exports = logger;
